f2b919067027b884c06798c52651bdb544ece33e
Pratik Kamble <62785111+Git-Pratik97@users.noreply.github.com>
2021-05-21 10:45:15 +0530

    Add files via upload

---
+++ number_pyramid.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i =1; i <= n; i++)
+    {
+        // for(int j =1; j <= n; j++)
+        // {
+        //     if (j <= n - (n -i))
+        //     {
+        //         cout << i;
+        //     }
+        //     else
+        //     {
+        //         cout << " ";
+        //     }
+        // }
+        for (int j=1; j <= i; j++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
